056746795b70e84aeec7ca332aa1ce09d0492eef
higginsgrafic <higginsgrafic@gmail.com>
2026-03-27 01:25:03 +0100

    chore: remove deleted files

--- src/components/AdidasCatalogPanel.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import MegaCatalogPanel from './MegaCatalogPanel.jsx';
-
-export default function AdidasCatalogPanel({ megaTileSize }) {
-  return <MegaCatalogPanel megaTileSize={megaTileSize} />;
-}
