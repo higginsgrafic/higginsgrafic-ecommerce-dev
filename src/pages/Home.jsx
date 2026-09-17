f880a714f3b1066bf271310f6dd544205d6c7e06
higginsgrafic <higginsgrafic@gmail.com>
2026-09-17 22:51:03 +0200

    Home: la hero i el menu d'icones de colleccions, 25 px mes amunt al vertical
    Nome ́s al vertical (tauleta en vertical): el menu d'icones passa de 109 a
    84 px de desplacament i la hero de +175 a +150 px. Horitzontal i desktop es
    queden igual.
    
    Comprovat: eslint net i build correcte.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -387,7 +387,8 @@ function Home() {
             justifyContent: 'center',
             gap: '44px',
             pointerEvents: 'auto',
-            transform: `translateY(${isPortraitTablet ? '109px' : '-26px'})`,
+            // Al vertical, el menu d'icones de col·leccions va 25 px mes amunt.
+            transform: `translateY(${isPortraitTablet ? '84px' : '-26px'})`,
           }}
         >
           {COLLECTIONS_MENU.map((c) => {
@@ -429,7 +430,7 @@ function Home() {
             gridColumn: '1 / 4',
             gridRow: '10 / 25',
             position: 'relative',
-            top: `calc(-5px - ${rowHeight / 2}px${isLandscapeTablet ? ' - 50px' : ''} - 50px${isLandscapeTablet ? ' + 50px' : ''}${isPortraitTablet ? ' + 175px' : ''})`,
+            top: `calc(-5px - ${rowHeight / 2}px${isLandscapeTablet ? ' - 50px' : ''} - 50px${isLandscapeTablet ? ' + 50px' : ''}${isPortraitTablet ? ' + 150px' : ''})`,
             width: 'calc(100% + 1px)',
             height: isPortraitTablet ? '430px' : 'calc(100% + 2px)',
             transform: 'scale(0.705)',
