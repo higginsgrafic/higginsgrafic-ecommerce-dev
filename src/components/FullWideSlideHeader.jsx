bd860d8d91f607ce613dfc1fdf9b8ec771b184ab
higginsgrafic <higginsgrafic@gmail.com>
2026-09-17 22:18:55 +0200

    Cadenat: la sortida acaba alhora que la pestanya
    La sortida arrencava 360 ms mes tard i durava 500 ms, o sigui que el cadenat
    arribava uns 600 ms despres que el panell hagues parat. Ara arrenca de
    seguida (retard 0) i dura 250 ms, que es el que triga el panell a fer
    l'ultim tram (de 398 a 424 px).
    
    Traça: el panell va de 398 a 424 entre t=2355 i t=2679 i el cadenat hi
    arriba a t=2732, al mateix moment.

--- src/components/FullWideSlideHeader.jsx
+++ src/components/FullWideSlideHeader.jsx
@@ -3108,7 +3108,9 @@ top: 'var(--globalHeaderTopOffset, 0px)', left: 'var(--rulerInset, 0px)', right:
             // queda just a sota del separador. L'animacio va a fora i el
             // transform de dins queda lliure per arrossegar-lo al vertical.
             top: `${lockBtnTop + CADE_BAIXADA_PX}px`,
-            animation: 'mega-cadenat-surt 500ms cubic-bezier(0.22, 1, 0.36, 1) 360ms both',
+            // La sortida dura el mateix que l'ultim tram del panell i comença de
+            // seguida, aixi el cadenat i la pestanya hi arriben alhora.
+            animation: 'mega-cadenat-surt 250ms cubic-bezier(0.22, 1, 0.36, 1) 0ms both',
             // Per sota del panell (z-[10000]) perque el cadenat en surti de sota.
             zIndex: 9999,
             pointerEvents: 'none',
