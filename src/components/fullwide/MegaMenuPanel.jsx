a017812f0e9fbb6f4b231946624b6d5c4e13b738
higginsgrafic <higginsgrafic@gmail.com>
2026-09-20 20:10:38 +0200

    Megaslide: les fletxes de la pagina 1, igualades amb el selector
    El contenidor de les fletxes passa del 75,9% al 90% de la casella, com el del
    selector: les fletxes fan 96,8 d'ample (abans 81,7), la mateixa amplada que el
    selector. L'alcada es la propia de la peca.
    
    Verificat: 462 proves i build OK.

--- src/components/fullwide/MegaMenuPanel.jsx
+++ src/components/fullwide/MegaMenuPanel.jsx
@@ -447,7 +447,7 @@ export default function MegaMenuPanel({
                           />
                         )}
                         fletxes={(
-                          <div style={{ width: '75.9%' }}>
+                          <div style={{ width: '90%' }}>
                           <FirstContactDibuix09Buttons
                             tileSize={96.8}
                             onPrev={() => { touchMegaPublicActivity?.(); }}
