a8a426068710902e189ed0b0f7582b1abaf04427
higginsgrafic <higginsgrafic@gmail.com>
2026-09-20 04:25:42 +0200

    PDP: graella de 5x3 amb la mida de casella de les fletxes
    Nova peca GraellaFletxes (5 columnes x 3 files) amb la casella de 44 px, que
    es la MATEIXA mida que la tile de les fletxes del rail (el rowHeight de
    CarouselArrows a la PDP). Muntada a la PDP, sota la TDP.
    
    Comprovat: graella 244x144, 15 caselles de 44x44, i la fletxa tambe fa 44x44.

--- src/pages/PdpPage.jsx
+++ src/pages/PdpPage.jsx
@@ -3,6 +3,7 @@ import { useLocation, Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import TambeRail from '@/pages/productRail/TambeRail';
 import CarouselArrows from '@/pages/productRail/CarouselArrows';
+import GraellaFletxes from '@/components/megaslide/GraellaFletxes.jsx';
 import Breadcrumbs from '@/components/Breadcrumbs';
 import { tdpImageFor, availableFinishesFor, defaultFinishFor } from '@/lib/pdpMockup';
 import EditableTextBox from '@/components/dev/EditableTextBox';
@@ -1011,6 +1012,14 @@ function PdpDesktop({ product }) {
             />
           </div>
           </div>
+        </PageBand>
+
+        <PageBand
+          type="graella"
+          fluid
+          style={{ marginTop: '100px', marginBottom: '32px' }}
+        >
+          <GraellaFletxes />
         </PageBand>
 
         <PageBand
